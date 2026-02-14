7c6104ada818f9139c9c8947a5d8c4d74de78bea
vivekpunde05 <your_email@example.com>
2026-02-14 14:10:38 +0530

    Remove Task 1 folder

--- Task 1/Error Message Detector.py
+++
@@ -1,8 +0,0 @@
-# List of system logs
-logs = ["INFO", "ERROR", "WARNING", "ERROR"]
-
-# Count the number of "ERROR" entries
-error_count = logs.count("ERROR")
-
-# Print the total error count
-print("Total 'ERROR' entries:",error_count)
--- Task 1/Simple Data Cleaner.py
+++
@@ -1,8 +0,0 @@
-# List of user names
-names = [" Alice ", "bob", " CHARLIE "]
-
-# Clean and standardize names
-cleaned_names = [name.strip().lower() for name in names]
-
-# Print the cleaned list
-print("Cleaned User Names:", cleaned_names)
